1ab2f5747f03d3a4b1a479e7a51125157113df5f
Tomas <arnotomas1@gmail.com>
2023-12-27 20:21:50 -0300

    sprint2

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const server = express();
+
+const PORT = 8080;
+
+const ready = () => console.log("server ready on port " + PORT);
+
+server.listen(PORT, ready);
+
+// MIDDLEWARES
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
