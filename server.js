8b1a7c23f36f3a3d12ab70a49c8157736bf68dee
Tomas <arnotomas1@gmail.com>
2023-12-27 20:57:58 -0300

    endpoint of users

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import UsersManager from "./data/fs/users.fs.js";
+
 const server = express();
 
 const PORT = 8080;
@@ -12,3 +14,61 @@ server.listen(PORT, ready);
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const all = await UsersManager.readFile();
+
+    if (all.length === 0) {
+      //   throw new Error("Not found users");
+      return res /*.status(404)*/
+        .json({
+          statusCode: 404,
+          message: "Not found users",
+        });
+    }
+
+    console.log(all);
+
+    return res.json({
+      statusCode: 200,
+      response: all,
+    });
+  } catch (e) {
+    console.log(e);
+
+    return res.json({
+      statusCode: 500,
+      message: e.message,
+    });
+  }
+});
+
+server.get("/api/users/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+
+    const one = await UsersManager.readOne(uid);
+
+    if (!one) {
+      return res.json({
+        statusCode: 404,
+        message: "User not found",
+      });
+    }
+
+    console.log(one);
+
+    return res.json({
+      statusCode: 200,
+      response: one,
+    });
+  } catch (e) {
+    console.log(e);
+
+    return res.json({
+      statusCode: 500,
+      message: e.message,
+    });
+  }
+});
