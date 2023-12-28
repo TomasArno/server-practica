f87ce5255fd3221bb76a5591331713d6dabee40e
Tomás <arnotomas1@gmail.com>
2023-12-27 21:39:48 -0300

    Sprint3 (#3)
    * endpoints of products (#1)
    
    * endpoint of users (#2)
    
    ---------
    
    Co-authored-by: Augusto <106772696+prados91@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import ProductsManager from "./data/fs/products.fs.js";
+import UsersManager from "./data/fs/users.fs.js";
 
 const server = express();
 
@@ -12,3 +14,109 @@ server.listen(PORT, ready);
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const all = await UsersManager.readFile();
+
+    if (all.length === 0) {
+      //   throw new Error("Not found users");
+      return res /*.status(404)*/
+        .json({
+          statusCode: 404,
+          message: "Not found users",
+        });
+    }
+
+    console.log(all);
+
+    return res.json({
+      statusCode: 200,
+      response: all,
+    });
+  } catch (e) {
+    console.log(e);
+
+    return res.json({
+      statusCode: 500,
+      message: e.message,
+    });
+  }
+});
+
+server.get("/api/users/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+
+    const one = await UsersManager.readOne(uid);
+
+    if (!one) {
+      return res.json({
+        statusCode: 404,
+        message: "User not found",
+      });
+    }
+
+    console.log(one);
+
+    return res.json({
+      statusCode: 200,
+      response: one,
+    });
+  } catch (e) {
+    console.log(e);
+
+    return res.json({
+      statusCode: 500,
+      message: e.message,
+    });
+  }
+});
+  
+server.get("/api/products", async (req, res) => {
+    try {
+        const all = await ProductsManager.readFile();
+
+        if (all.lenght === 0) {
+            return res.json({
+                statusCode: 404,
+                message: "Not found products",
+            });
+        }
+        return res.json({
+            statusCode: 200,
+            response: all,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            statusCode: 500,
+            message: error.message,
+        });
+    }
+});
+
+server.get("/api/products/:pid", async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const one = await ProductsManager.readOne(pid);
+
+        if (!one) {
+            return res.json({
+                statusCode: 404,
+                message: "Not found product",
+            });
+        } else {
+            return res.json({
+                statusCode: 200,
+                response: one,
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            statusCode: 500,
+            message: error.message,
+        });
+    }
+});
