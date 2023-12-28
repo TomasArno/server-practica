4b3d9b147b7a67b1a12fc70f484ccfb66806c902
Augusto <106772696+prados91@users.noreply.github.com>
2023-12-27 21:24:57 -0300

    endpoints of products (#1)

--- data/fs/products.fs.js
+++ data/fs/products.fs.js
@@ -3,101 +3,102 @@ import { existsSync, writeFileSync } from "node:fs";
 import crypto from "node:crypto";
 
 class ProductManager {
-  static #path = "./data/fs/files/products.json";
+    static #path = "./data/fs/files/products.json";
 
-  constructor() {
-    this.init();
-  }
-
-  init() {
-    if (!existsSync(ProductManager.#path)) {
-      const data = JSON.stringify([], null, 2);
-
-      writeFileSync(ProductManager.#path, data);
-    }
-  }
-
-  async create(data) {
-    const propsList = ["title", "photo", "price", "stock"];
-    const keyList = Object.keys(data);
-
-    const missingProps = [];
-
-    for (let i = 0; i < propsList.length; i++) {
-      !keyList.includes(propsList[i]) ? missingProps.push(propsList[i]) : null;
+    constructor() {
+        this.init();
     }
 
-    if (missingProps.length) {
-      console.log(`Propiedades faltantes: ${missingProps.join(" ")}`);
-    } else {
-      const { title, photo, price, stock } = data;
+    init() {
+        if (!existsSync(ProductManager.#path)) {
+            const data = JSON.stringify([], null, 2);
 
-      const products = await this.readFile();
-
-      products.push({
-        id: crypto.randomBytes(12).toString("hex"),
-        title,
-        photo,
-        price,
-        stock,
-      });
-
-      try {
-        await fs.writeFile(
-          ProductManager.#path,
-          JSON.stringify(products, null, 2)
-        );
-
-        return products[products.length - 1];
-      } catch (e) {
-        throw e.error;
-      }
+            writeFileSync(ProductManager.#path, data);
+        }
     }
-  }
 
-  async readFile() {
-    try {
-      const products = JSON.parse(
-        await fs.readFile(ProductManager.#path, {
-          encoding: "utf-8",
-        })
-      );
+    async create(data) {
+        const propsList = ["title", "photo", "price", "stock"];
+        const keyList = Object.keys(data);
 
-      return products;
-    } catch (e) {
-      throw e.message;
+        const missingProps = [];
+
+        for (let i = 0; i < propsList.length; i++) {
+            !keyList.includes(propsList[i]) ? missingProps.push(propsList[i]) : null;
+        }
+
+        if (missingProps.length) {
+            console.log(`Propiedades faltantes: ${missingProps.join(" ")}`);
+        } else {
+            const { title, photo, price, stock } = data;
+
+            const products = await this.readFile();
+
+            products.push({
+                id: crypto.randomBytes(12).toString("hex"),
+                title,
+                photo,
+                price,
+                stock,
+            });
+
+            try {
+                await fs.writeFile(ProductManager.#path, JSON.stringify(products, null, 2));
+
+                return products[products.length - 1];
+            } catch (e) {
+                throw e.error;
+            }
+        }
     }
-  }
 
-  async readOne(id) {
-    try {
-      const products = await this.readFile();
+    async readFile() {
+        try {
+            const products = JSON.parse(
+                await fs.readFile(ProductManager.#path, {
+                    encoding: "utf-8",
+                })
+            );
 
-      return products.find((el) => el.id == id) ?? [];
-    } catch (e) {
-      throw e.message;
+            return products;
+        } catch (e) {
+            throw e.message;
+        }
     }
-  }
 
-  async destroy(id) {
-    try {
-      const products = await this.readFile();
-      const newList = products.filter((el) => el.id !== id);
+    async readOne(id) {
+        try {
+            const products = await this.readFile();
 
-      if (products.length == newList.length) return false;
-
-      await fs.writeFile(
-        ProductManager.#path,
-        JSON.stringify(newList, null, 2)
-      );
-
-      return true;
-    } catch (error) {
-      throw e.error;
+            return products.find((el) => el.id == id);
+        } catch (e) {
+            throw e.message;
+        }
     }
-  }
+
+    async destroy(id) {
+        try {
+            const products = await this.readFile();
+            const newList = products.filter((el) => el.id !== id);
+
+            if (products.length == newList.length) return false;
+
+            await fs.writeFile(ProductManager.#path, JSON.stringify(newList, null, 2));
+
+            return true;
+        } catch (error) {
+            throw e.error;
+        }
+    }
 }
 
 const ProductsManager = new ProductManager();
 
+/*ProductsManager.create({
+    title: "producto1",
+    photo: "photo1",
+    price: 1,
+    stock: 1000,
+});
+*/
 export default ProductsManager;
