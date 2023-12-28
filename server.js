5913b0517793188bdd5d3e7a95f1da1205dbae22
Augusto <augustoprados91@gmail.com>
2023-12-27 21:21:36 -0300

    endpoints of products

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import ProductsManager from "./data/fs/products.fs.js";
 
 const server = express();
 
@@ -12,3 +13,51 @@ server.listen(PORT, ready);
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+
+server.get("/api/products", async (req, res) => {
+    try {
+        const all = await ProductsManager.readFile();
+
+        if (all.lenght === 0) {
+            return res.json({
+                statusCode: 404,
+                message: "Not found products",
+            });
+        }
+        return res.json({
+            statusCode: 200,
+            response: all,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            statusCode: 500,
+            message: error.message,
+        });
+    }
+});
+
+server.get("/api/products/:pid", async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const one = await ProductsManager.readOne(pid);
+
+        if (!one) {
+            return res.json({
+                statusCode: 404,
+                message: "Not found product",
+            });
+        } else {
+            return res.json({
+                statusCode: 200,
+                response: one,
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            statusCode: 500,
+            message: error.message,
+        });
+    }
+});
